5a69650149e222b97b5fa8e99488fe181463410b
Atifsid <atifsiddiqui35@gmail.com>
2024-08-13 23:29:43 +0530

    chore: make header responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,16 @@
-import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="bg-primary">
-      <div className="center-content items-center common-padding h-16">
-        <div className="cursor-pointer">
-          <h3 className="text-white font-mono font-bold text-xl italic">
-            SHOPiT
-          </h3>
-          <h6 className="text-xs italic text-white">Buy anything</h6>
-        </div>
-        <h3 className="text-white cursor-pointer">Cart</h3>
-      </div>
-    </div>
+    <header className="flex flex-row items-center justify-between sm:justify-around py-3 bg-primary px-10">
+      <Link href={"/"} className="cursor-pointer">
+        <h3 className="text-white font-mono font-bold text-xl italic">
+          SHOPiT
+        </h3>
+        <h6 className="text-xs italic text-white">Buy anything</h6>
+      </Link>
+      <h3 className="text-white cursor-pointer">Cart</h3>
+    </header>
   );
 };
 
