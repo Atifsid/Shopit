217378dabe8156c6d2fbe76594d2f033f48c8b0c
Atifsid <atifsiddiqui35@gmail.com>
2024-08-14 10:18:37 +0530

    chore: add cart icon and item count in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="flex flex-row items-center justify-between sm:justify-around py-3 bg-primary px-10">
+    <header className="flex flex-row items-center justify-between sm:justify-around py-1 bg-primary px-10">
       <Link href={"/"} className="cursor-pointer">
         <h3 className="text-white font-mono font-bold text-xl italic">
           SHOPiT
         </h3>
         <h6 className="text-xs italic text-white">Buy anything</h6>
       </Link>
-      <h3 className="text-white cursor-pointer">Cart</h3>
+      <button className="py-3 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
+        <FaShoppingCart className="text-white text-2xl" />
+        <span className="absolute inset-0 object-right-top -mr-6">
+          <div className="inline-flex items-center px-1.5 py-0.5 rounded-full text-xs font-semibold bg-red-500 text-white">
+            0
+          </div>
+        </span>
+      </button>
     </header>
   );
 };
