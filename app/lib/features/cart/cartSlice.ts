bcf239e752972d167a300c008e34cf9a2828742b
Atifsid <atifsiddiqui35@gmail.com>
2024-08-14 23:22:44 +0530

    chore: add comments to cartSlice and update logic for decreaseQuantity

--- app/lib/features/cart/cartSlice.ts
+++ app/lib/features/cart/cartSlice.ts
@@ -4,56 +4,84 @@ import { CartProduct } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface CartState {
-    cartSize: number;
-    cartItems: CartProduct[]
+  cartSize: number;
+  cartItems: CartProduct[];
 }
 
 const initialState: CartState = {
-    cartSize: 0,
-    cartItems: []
-}
+  cartSize: 0,
+  cartItems: [],
+};
 
 export const counterSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addProductToCart: (state, action) => {
-            const cartProductIdx = state.cartItems.findIndex(item => item.id === action.payload.id);
-            if (cartProductIdx === -1) {
-                state.cartItems.push({
-                    ...action.payload,
-                    quantity: 1
-                });
-                state.cartSize += 1;
-            } else {
-                const cartItem = state.cartItems[cartProductIdx];
-                state.cartItems[cartProductIdx].quantity! += 1;
-                state.cartItems.splice(cartProductIdx, 1, cartItem);
-            }
-            state.cartSize = state.cartItems.length;
-        },
-        removeProductFromCart: (state, action) => {
-            const cartProductIdx = state.cartItems.findIndex(item => item.id === action.payload.id);
-            if (cartProductIdx != -1) {
-                state.cartItems.splice(cartProductIdx, 1);
-            }
-            state.cartSize = state.cartItems.length;
-        },
-        decreaseQuantity: (state, action) => {
-            const cartProductIdx = state.cartItems.findIndex(item => item.id === action.payload.id);
-            if (cartProductIdx != -1) {
-                const cartItem = state.cartItems[cartProductIdx];
-                if (cartItem.quantity! === 1) {
-                    state.cartItems.splice(cartProductIdx, 1);
-                } else {
-                    cartItem.quantity! -= 1;
-                    state.cartItems.splice(cartProductIdx, 1, cartItem);
-                }
-            }
-            state.cartSize = state.cartItems.length;
-        }
-    }
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    // adds item to cart
+    addProductToCart: (state, action) => {
+      const cartProductIdx = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
 
-export const { addProductToCart, removeProductFromCart, decreaseQuantity } = counterSlice.actions;
+      // increase item quantity if item is already present.
+      if (cartProductIdx === -1) {
+        state.cartItems.push({
+          ...action.payload,
+          quantity: 1,
+        });
+        state.cartSize += 1;
+      }
+      // else add new item
+      else {
+        const cartItem = state.cartItems[cartProductIdx];
+        state.cartItems[cartProductIdx].quantity! += 1;
+        state.cartItems.splice(cartProductIdx, 1, cartItem);
+      }
+      // update cart size
+      state.cartSize = state.cartItems.length;
+    },
+
+    // remove product from cart only if item is present in the cart.
+    removeProductFromCart: (state, action) => {
+      const cartProductIdx = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (cartProductIdx != -1) {
+        state.cartItems.splice(cartProductIdx, 1);
+      }
+
+      // update cart size
+      state.cartSize = state.cartItems.length;
+    },
+
+    // decrement item quantity only if item is present in the cart.
+    decreaseQuantity: (state, action) => {
+      const cartProductIdx = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (cartProductIdx != -1) {
+        const cartItem = state.cartItems[cartProductIdx];
+
+        // if cart item's quantity is truthy.
+        if (cartItem.quantity) {
+          // if cart item's quantity greater than one then decrement by 1.
+          if (cartItem.quantity > 1) {
+            cartItem.quantity -= 1;
+            state.cartItems.splice(cartProductIdx, 1, cartItem);
+          }
+          // else remove the item. i.e, cartItem.quantity <= 1.
+          else {
+            state.cartItems.splice(cartProductIdx, 1);
+          }
+        }
+      }
+
+      // update cart size
+      state.cartSize = state.cartItems.length;
+    },
+  },
+});
+
+export const { addProductToCart, removeProductFromCart, decreaseQuantity } =
+  counterSlice.actions;
 export default counterSlice.reducer;
